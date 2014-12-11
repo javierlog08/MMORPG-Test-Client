a821cdab05ad9f1841d03ff9d14fc1bb9e3efabe
javierlog08 <javierlog08@gmail.com>
2014-12-11 11:32:31 -0500

    On Progress: Login Form Submit

--- app/game/ui/login/Login.js
+++ app/game/ui/login/Login.js
@@ -1,13 +1,22 @@
 define([
-	'jquery'
-],function(jQuery){
+	'jquery',
+	'libs/jquery-form/jquery.form'
+],function(jQuery,jqf){
 
 	var Login = function(){
 		this.render = function()  {
 			jQuery("#game").append('<div id="login" style="width: 30%;height: 30%; position:absolute; z-index: 1; left:0;right:0;top:30%; margin-left:auto;margin-right:auto;" ></div>');
-			jQuery("#login").load("game/ui/login/login.html");
+			jQuery("#login").load("game/ui/login/login.html",function(){
+				$('#form').ajaxForm({
 
-			$('#login').show("fade");
+					dataType:  'json',
+
+					success:   function(data) {
+						console.log(data);
+					}
+
+				});
+			});
 		}
 	}
 
